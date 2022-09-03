dee8bec7080122ddf384ff73aa692ffbd5ef6c0a
AndersonBarbosaDeFreitas <anderson.barbosa@dcx.ufpb.br>
2022-09-03 11:50:33 -0300

    Removendo opções de dashboard da navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,8 +1,6 @@
 import { UserModel } from './../../models/UserModel';
 import { AuthService } from './../../services/auth.service';
 import { StorageService } from './../../services/storage.service';
-import { Router } from '@angular/router';
-import { Location } from '@angular/common';
 import { Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
 
 @Component({
@@ -27,16 +25,11 @@ export class NavbarComponent implements OnInit {
   @ViewChild('menu') menu!: ElementRef;
 
   constructor(
-    private location: Location,
-    private router: Router,
     private storageService: StorageService,
     private authService: AuthService
   ) { }
 
   ngOnInit(): void {
-    console.log(this.router.url);
-    this.getCurrentPage();
-
     this.isLoggedIn = this.storageService.isLoggedIn();
     if (this.isLoggedIn) {
       this.currentUser = this.storageService.getUser();
@@ -47,31 +40,9 @@ export class NavbarComponent implements OnInit {
     }
   }
 
-  getCurrentPage(): void {
-    this.router.events.subscribe((_) => {
-      this.routerPath = this.location.path();
-      if (this.location.path().includes('/login')) {
-        this.isLoginPage = true;
-      } else {
-        this.isLoginPage = false;
-      }
-    });
-  }
-
-  checkPathIsEqual(path: string): boolean {
-    return this.routerPath == path;
-  }
-
   menuToggle(): void {
     this.menuActive = !this.menuActive;
   }
-
-  // @HostListener('document:mousedown', ['$event'])
-  // onGlobalClick(event: { target: any; }): void {
-  //   if (!this.toggleButton.nativeElement.contains(event.target) && !this.menu.nativeElement.contains(event.target)) {
-  //     this.menuActive = false;
-  //   }
-  // }
 
   isLogged(): boolean {
     return false;
