a783dfe57992a1460bcddf85afd0d1c9188b372a
AndersonBarbosaDeFreitas <anderson.barbosa@dcx.ufpb.br>
2022-09-03 11:56:02 -0300

    Adicionando verificação se deve ou não exibir a navbar ou sidebar(logado)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,38 @@
 import { Router } from '@angular/router';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'qtarolando-manager';
+
+  routerPath: string = "";
+  showNavBar: boolean = true;
+  showSideBar: boolean = false;
+
+  constructor(private router: Router, private location: Location) { }
+
+  ngOnInit(): void {
+    this.getCurrentPage();
+  }
+
+  getCurrentPage(): void {
+    this.router.events.subscribe((_) => {
+      this.routerPath = this.location.path();
+      if (this.routerPath.includes('dashboard')) {
+        this.showNavBar = false;
+        this.showSideBar = true;
+      } else if (this.routerPath.includes('login')) {
+        this.showNavBar = false;
+        this.showSideBar = false;
+      } else {
+        this.showNavBar = true;
+        this.showSideBar = false;
+      }
+    });
+  }
 }
