13921cc7f0808c037d48c2fbc3a359f09769517f
AndersonBarbosaDeFreitas <anderson.barbosa@dcx.ufpb.br>
2022-09-03 11:56:50 -0300

    Alteração do layout da sidebar e adição de responsividade

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,6 +1,7 @@
 import { Router } from '@angular/router';
 import { Component, Input, OnInit } from '@angular/core';
-import { PagesEnum } from 'src/app/utils/constants';
+import { UserModel } from 'src/app/models/UserModel';
+import { StorageService } from 'src/app/services/storage.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -9,18 +10,46 @@ import { PagesEnum } from 'src/app/utils/constants';
 })
 export class SidebarComponent implements OnInit {
 
-  @Input() page!: PagesEnum;
+  @Input() routerPath!: string;
 
-  constructor(private router: Router) { }
+  currentUser!: UserModel;
+  roles: string[] = [];
+  showUsersOptions: boolean = false;
+
+  constructor(private storageService: StorageService,) { }
 
   ngOnInit(): void {
+    console.log(this.routerPath);
+    this.showMenu('nav-toggle', 'navbar');
+
+    if (this.storageService.isLoggedIn()) {
+      this.currentUser = this.storageService.getUser();
+      this.roles = this.currentUser.roles;
+
+      this.showUsersOptions = this.roles.includes('ROLE_ADMIN');
+    }
   }
 
+  showMenu(toggleId: string, navId: string): void {
+    const toggle = document.getElementById(toggleId),
+      nav = document.getElementById(navId);
 
+    if (toggle && nav) {
+      toggle.addEventListener('click', () => {
+        nav.classList.toggle('show-menu');
+        if (toggle.classList.contains('eva-menu-outline')) {
+          toggle.classList.add('eva-close-outline');
+          toggle.classList.remove('eva-menu-outline');
+        } else {
+          toggle.classList.add('eva-menu-outline');
+          toggle.classList.remove('eva-close-outline');
+        }
+      });
+    }
+  }
 
-  toggleMenu() {
-    const sidebar = document?.querySelector(".sidebar");
-    sidebar?.classList.toggle("close")
+  containsRouterPath(path: string): boolean {
+    return this.routerPath.includes(path);
   }
 
 }
