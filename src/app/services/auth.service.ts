330c0bf3d007ea3b7f5f82a5925b4250c1420e19
AndersonBarbosaDeFreitas <anderson.barbosa@dcx.ufpb.br>
2022-11-30 21:48:08 -0300

    Correção no serviço de login de usuário

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,38 +1,35 @@
+import { environment } from './../../environments/environment';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { UserRequestModel } from '../models/UserRequestModel';
-
-const AUTH_API = 'http://localhost:8080/api/auth/';
-
-let httpOptions = {
-  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
-}
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
+  private readonly AUTH_API = `${environment.API}/auth/`;
+
   constructor(private http: HttpClient) { }
 
   login(email: string, password: string): Observable<any> {
-    const options = {
-      headers: new HttpHeaders({ 'Content-Type': 'application/json', Authorization: 'Basic ' + btoa(`${email}:${password}`) })
-    }
-    return this.http.post(AUTH_API + 'login', null, options);
+    return this.http.post(this.AUTH_API + 'login', {
+      email,
+      password: password
+    });
   }
 
   register(user: UserRequestModel): Observable<any> {
-    return this.http.post(AUTH_API + 'signup', {
+    return this.http.post(this.AUTH_API + 'signup', {
       username: user.username,
       email: user.email,
       password: user.password
-    }, httpOptions);
+    });
   }
 
   logout(): Observable<any> {
-    return this.http.post(AUTH_API + 'signout', {}, httpOptions);
+    return this.http.post(this.AUTH_API + 'signout', {});
   }
 
 }
