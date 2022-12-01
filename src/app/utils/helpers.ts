692d023c45631d9cb23c809de94c77e5d0e5ec56
AndersonBarbosaDeFreitas <anderson.barbosa@dcx.ufpb.br>
2022-11-30 21:54:54 -0300

    Criando funções utilitárias

--- src/app/utils/helpers.ts
+++ src/app/utils/helpers.ts
@@ -1,7 +1,33 @@
+import { ActivatedRoute, Router } from "@angular/router";
+import { CategoriesEnum, ModalitiesEnum } from "./constants";
+
 const randomNum = () => Math.floor(Math.random() * (235 - 52 + 1) + 52);
 
 const randomRGB = () => `rgb(${randomNum()}, ${randomNum()}, ${randomNum()})`;
 
+const getCategories = (): Array<string> => {
+  const keys = Object.keys(CategoriesEnum);
+  return keys.map(el => Object(CategoriesEnum)[el]).filter(value => value != '');
+}
+
+const getModalities = (): Array<string> => {
+  const keys = Object.keys(ModalitiesEnum);
+  return keys.map(el => Object(ModalitiesEnum)[el]).filter(value => value != '');
+}
+
+const setQueryParms = (router: Router, activatedRoute: ActivatedRoute, queryParams: {}) => {
+  router.navigate(
+    [],
+    {
+      relativeTo: activatedRoute,
+      queryParams: queryParams,
+      queryParamsHandling: 'merge',
+    });
+}
+
 export {
-  randomRGB
+  randomRGB,
+  getCategories,
+  getModalities,
+  setQueryParms
 }
